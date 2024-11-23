5d9c21698f6848eb8c473fb8cd3c9160dbc305be
Ryad Juliano <ryadi.putra@sociolla.com>
2024-11-23 21:57:37 +0700

    feat : controller category

---
+++ application/controllers/api/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+// use namespace
+use Restserver\Libraries\REST_Controller;
+
+class Category extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+    
+         $this->load->database();
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['list_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+    }
+
+    public function list_get()
+    {
+        $tenant = $this->db->get('sim_group_category')->result();
+        // $this->response($kontak, 200);
+        $this->response($tenant, REST_Controller::HTTP_OK); 
+        
+    }
+
+  
+
+}
