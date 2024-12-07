a7c2e994e938aae3347dfe42c68dfacdce86926f
Ryad Juliano <ryadi.putra@sociolla.com>
2024-12-07 19:35:59 +0700

    feat : trx inv

--- application/controllers/api/Transaction.php
+++ application/controllers/api/Transaction.php
@@ -2,11 +2,12 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
 require APPPATH . '/libraries/REST_Controller.php';
 
 // use namespace
 use Restserver\Libraries\REST_Controller;
-
 
 class Transaction extends REST_Controller {
 
@@ -23,51 +24,74 @@ class Transaction extends REST_Controller {
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    public function allinvoice_get($customer_id = null, $check = false, $user_id = null)
+    public function createinvoice_post()
     {
+
+        $rawPayload = file_get_contents('php://input');
+    
+        // Step 2: Decode the JSON into an associative array
+        $data = json_decode($rawPayload, true);
+        echo $datal;
+
+
+        // if ($this->db->insert('sales', $data)) {
+        //     $sale_id = $this->db->insert_id();
+
+        //     foreach ($items as $item) {
+        //         $item['sale_id'] = $sale_id;
+        //         $this->db->insert('sale_items', $item);
+        //     }
+
+        //     if ($data['status'] == $this->lang->line('paid') || $data['status'] == 'paid') {
+        //         $adata = [
+        //             'date'        => $data['date'],
+        //             'invoice_id'  => $sale_id,
+        //             'customer_id' => $data['customer_id'],
+        //             'amount'      => ($data['total'] + $data['shipping']),
+        //             'note'        => $this->lang->line('paid_nett'),
+        //             'user'        => $this->session->userdata('user_id')
+        //         ];
+        //         $this->db->insert('payment', $adata);
+        //         $this->db->update('sales', ['paid' => ($data['total'] + $data['shipping'])], ['id' => $sale_id]);
+        //     }
+
+        //     return true;
+        // }
+
+        // return false;
+        // $this->db->select('sim_customers.address, sim_customers.cf1, sim_customers.phone, sim_customers.name, sim_customers_details.lokasi');
+        // $this->db->from('sim_customers');
+        // $this->db->join('sim_customers_details', 'sim_customers.phone = sim_customers_details.phone'); // Adjust column names for joining
+        // // $this->db->limit(10); 
+        // $this->db->group_by(['sim_customers.phone', 'sim_customers.name']);
+        // $q = $this->db->get();
         
-        $sales = $this->db
-        ->select("{$this->db->dbprefix('sales')}.id as sid, 
-                    date, 
-                    ballon_status, 
-                    company_name, 
-                    reference_no, 
-                    CONCAT({$this->db->dbprefix('users')}.first_name, ' ', {$this->db->dbprefix('users')}.last_name) as user, 
-                    customer_name, 
-                    grand_total, 
-                    paid, 
-                    (grand_total - COALESCE(paid, 0)) as balance, 
-                    customers.phone as telephone, 
-                    due_date, 
-                    status, 
-                    recurring, 
-                    {$this->db->dbprefix('sales')}.customer_id as cid, 
-                    company_id as bid")
-        ->from('sales')
-        ->join('users', 'users.id = sales.user', 'LEFT')
-        ->join('customers', 'customers.phone = users.phone', 'LEFT')
-        ->group_by('sales.id');
-    
-    // Filter berdasarkan parameter
-    if ($customer_id) {
-        $this->db->where('sales.customer_id', $customer_id);
-    }
-    
-    if ($check) {
-        $this->db->where('sales.user', $user_id);
-    }
-    
-    // Tambahkan pengurutan berdasarkan tanggal terbaru
-    $this->db->order_by('sales.date', 'DESC'); // Mengurutkan berdasarkan tanggal (baru di atas)
-    
-    // Menambahkan limit
-    $this->db->limit(30); // Batasi hasil query (misalnya 5 data)
-    
-    // Eksekusi query
-    $result = $this->db->get()->result();
-    
-    // Mengirimkan respons dalam format JSON
-    $this->response($result, REST_Controller::HTTP_OK);    
+        // if ($q->num_rows() > 0) {
+        //     $result = [];
+        //     $no = 1;
+        //     foreach ($q->result() as $row) {
+        //         $result[] = [
+        //             'id' => $no++,
+        //             'name' => $row->name,
+        //             'phone' => $row->phone,
+        //             'addresses' => [$row->lokasi, $row->address]
+        //         ];
+        //     }
+
+        //     $message = [
+        //         "data" => $result,
+        //         "success" => true
+        //     ];
+        // } else {
+        //     $message = [
+        //         "data" => [],
+        //         "success" => false,
+        //         "message" => "No records found."
+        //     ];
+        // }
+        
+        $this->response($message, REST_Controller::HTTP_OK); 
     }
 
+    
 }
