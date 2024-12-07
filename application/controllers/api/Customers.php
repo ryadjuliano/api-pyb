b87633d2d05816fb96ce05c68866f4adefd1ab32
Ryad Juliano <ryadi.putra@sociolla.com>
2024-12-07 10:48:57 +0700

    feat : search cust

--- application/controllers/api/Customers.php
+++ application/controllers/api/Customers.php
@@ -54,7 +54,52 @@ class Customers extends REST_Controller {
             ];
         }
         
-        $this->response($message, REST_Controller::HTTP_OK);
-          
+        $this->response($message, REST_Controller::HTTP_OK); 
+    }
+
+    public function filterlist_post() {
+       
+        $limit = $this->post('_limit') ?: 10; // Default to 10 items per page if not provided
+        $phone_number = $this->post('phone_number');
+        $name = $this->post('name');
+    
+    
+        if (!empty($phone_number)) {
+            $this->db->like('sim_customers_details.phone', $phone_number);
+        }
+        if (!empty($name)) {
+            $this->db->like('sim_customers_details.name', $name);
+        }
+        
+
+        $this->db->select('sim_customers.address, sim_customers.cf1, sim_customers_details.phone, sim_customers_details.name, sim_customers_details.lokasi');
+        $this->db->from('sim_customers');
+        $this->db->join('sim_customers_details', 'sim_customers.phone = sim_customers_details.phone'); // Adjust column names for joining
+        $this->db->limit($limit); 
+        $q = $this->db->get();
+        
+        if ($q->num_rows() > 0) {
+            $result = [];
+            foreach ($q->result() as $row) {
+                $result[] = [
+                    'name' => $row->name,
+                    'phone' => $row->phone,
+                    'addresses' => [$row->lokasi, $row->address]
+                ];
+            }
+
+            $message = [
+                "data" => $result,
+                "success" => true
+            ];
+        } else {
+            $message = [
+                "data" => [],
+                "success" => false,
+                "message" => "No records found."
+            ];
+        }
+        
+        $this->response($message, REST_Controller::HTTP_OK); 
     }
 }
