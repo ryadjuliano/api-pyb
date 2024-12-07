0c385420253b92855ad3223d610dea0794b181aa
Ryad Juliano <ryadi.putra@sociolla.com>
2024-12-07 21:35:10 +0700

    feat : trx inv api and list inv

--- application/controllers/api/Transaction.php
+++ application/controllers/api/Transaction.php
@@ -2,12 +2,11 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// This can be removed if you use __autoload() in config.php OR use Modular Extensions
-/** @noinspection PhpIncludeInspection */
 require APPPATH . '/libraries/REST_Controller.php';
 
 // use namespace
 use Restserver\Libraries\REST_Controller;
+
 
 class Transaction extends REST_Controller {
 
@@ -22,6 +21,53 @@ class Transaction extends REST_Controller {
         $this->methods['list_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+    }
+
+    public function allinvoice_get($customer_id = null, $check = false, $user_id = null)
+    {
+        
+        $sales = $this->db
+        ->select("{$this->db->dbprefix('sales')}.id as sid, 
+                    date, 
+                    ballon_status, 
+                    company_name, 
+                    reference_no, 
+                    CONCAT({$this->db->dbprefix('users')}.first_name, ' ', {$this->db->dbprefix('users')}.last_name) as user, 
+                    customer_name, 
+                    grand_total, 
+                    paid, 
+                    (grand_total - COALESCE(paid, 0)) as balance, 
+                    customers.phone as telephone, 
+                    due_date, 
+                    status, 
+                    recurring, 
+                    {$this->db->dbprefix('sales')}.customer_id as cid, 
+                    company_id as bid")
+        ->from('sales')
+        ->join('users', 'users.id = sales.user', 'LEFT')
+        ->join('customers', 'customers.phone = users.phone', 'LEFT')
+        ->group_by('sales.id');
+    
+    // Filter berdasarkan parameter
+    if ($customer_id) {
+        $this->db->where('sales.customer_id', $customer_id);
+    }
+    
+    if ($check) {
+        $this->db->where('sales.user', $user_id);
+    }
+    
+    // Tambahkan pengurutan berdasarkan tanggal terbaru
+    $this->db->order_by('sales.date', 'DESC'); // Mengurutkan berdasarkan tanggal (baru di atas)
+    
+    // Menambahkan limit
+    $this->db->limit(30); // Batasi hasil query (misalnya 5 data)
+    
+    // Eksekusi query
+    $result = $this->db->get()->result();
+    
+    // Mengirimkan respons dalam format JSON
+    $this->response($result, REST_Controller::HTTP_OK);    
     }
 
     public function createinvoice_post()
@@ -117,5 +163,5 @@ class Transaction extends REST_Controller {
         $this->response($message, REST_Controller::HTTP_OK); 
     }
 
-    
+
 }
